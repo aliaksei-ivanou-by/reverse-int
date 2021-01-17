4279571a4cdc6f9bc41751b96a2c0022a33fb503
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-01-17 12:29:06 +0300

    feat: add function reverse(int)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) // 123
+{
+	var current;
+	var output = 0
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	while (n)
+	{
+		current = n % 10; // 123 % 10 = 3, 12 % 10 = 2, 1 % 10 = 1
+        output = (output * 10) + current; // 3, 30 + 2 = 32, 320 + 1 = 321
+        n = n / 10 | 0; // 123 / 10 = 12, 12 / 10 = 1, 1 / 10 = 0
+    }
+	return output // 321
 }
